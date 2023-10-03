fd13a5678bf679e8d705ec1a5555329012965ab4
gunwunbun <jvanhouts@protonmail.com>
2023-10-03 13:10:34 +0200

    finalised

--- BroadwayBB.Common/Entities/Attendees/Collider/ColliderStrategyBase.cs
+++ BroadwayBB.Common/Entities/Attendees/Collider/ColliderStrategyBase.cs
@@ -6,8 +6,8 @@ public abstract class ColliderStrategyBase : IColliderStrategy
 {
     private readonly int _minimumCollisionDistance = 1;
     public Rectangle Size { get; private set; }
-    
-    public ColliderStrategyBase(Rectangle size) => Size = size;
+
+    protected ColliderStrategyBase(Rectangle size) => Size = size;
 
     public abstract void HandleCollision(List<IAttendee> attendees);
 
@@ -15,11 +15,8 @@ public abstract class ColliderStrategyBase : IColliderStrategy
     {
         double dx = Math.Abs(one.Movement.GridPosX - two.Movement.GridPosX);
         double dy = Math.Abs(one.Movement.GridPosY - two.Movement.GridPosY);
-
+        
         bool isColliding = dx <= _minimumCollisionDistance && dy <= _minimumCollisionDistance;
-            
-        if (isColliding)
-            Console.WriteLine("Colliding: " + one.Movement.GridPosX + "|" + one.Movement.GridPosY + " -- " + two.Movement.GridPosX + "|" + two.Movement.GridPosY);
         
         one.Movement.IsColliding = isColliding;
         two.Movement.IsColliding = isColliding;
